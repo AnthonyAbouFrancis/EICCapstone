55e4537d8ad725bc1f7de4387ee50b2a72a80bed
ievafil <92386141+ievafil@users.noreply.github.com>
2021-11-11 11:21:38 +0000

    Update products.js

--- BackEndDev/routes/products.js
+++ BackEndDev/routes/products.js
@@ -3,6 +3,20 @@ var router = express.Router();
 var products = require("../mockData.json/products.json");
 
 
+module.exports = function () {
+	router
+		.route('/')
+		.get((req, res, next) => {
+			res.json(products);
+		})
+		.post((req, res) => {
+			// validate and post new products
+		});
+
+	return router;
+};
+
+
 
 
 
