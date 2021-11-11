b4fb19ef85683dc4767bc5b8d0bcadd76ee295e4
ievafil <92386141+ievafil@users.noreply.github.com>
2021-11-11 13:56:02 +0000

    update with products api

--- BackEndDev/app.js
+++ BackEndDev/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var customerRouter = require('./routes/customers');
 var orderRouter = require('./routes/orders');
+var productsRouter = require('./routes/products'); //added for products
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/customers', customerRouter());
 app.use('/api/orders', orderRouter);
+app.use('/api/products', productsRouter()); //added for products
 app.get('/', function (req, res) {
 	res.send('<h1>hello world</h1>')
 })
